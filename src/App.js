2b35367d0aebe479f25d0f5e2124622c269e9924
khazzi <akandeowoo@gmail.com>
2024-01-23 19:00:30 +0100

    latest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {Footer, Blog, Features, Header, Possibility, Whatgpt3} from './container'
+import {Navbar, Brand, Cta} from './components'
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='App'>
+      <div className='gradient__bg'>
+      <Navbar />
+      <Header />
+      </div>
+      <Brand />
+      <Whatgpt3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
